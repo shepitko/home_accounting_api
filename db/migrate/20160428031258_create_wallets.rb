9bab721243bc82f8db49dce507096cc5402590c0
shepitko <ya.shepitko@gmail.com>
2016-04-28 06:33:23 +0300

    were generated model and migration

---
+++ db/migrate/20160428031258_create_wallets.rb
@@ -0,0 +1,12 @@
+class CreateWallets < ActiveRecord::Migration
+  def change
+    create_table :wallets do |t|
+      t.decimal :price, :precision => 10, :scale => 2
+      t.date :date
+      t.references :category, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
