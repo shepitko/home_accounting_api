4b9622f654c723f96617acaaa79eabc7b4a27664
shepitko <ya.shepitko@gmail.com>
2016-06-09 12:08:54 +0300

    seeds file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+spend = [
+	['Продукты', 'shopping-basket '],
+	['Одежда', 'black-tie'],
+	['Техника', 'camera-retro'],
+	['Подарки', 'gift'],
+	['Бытовая химия', 'flask'],
+	['Рестораны', 'glass or glass'],
+	['Путешествия', 'compass'],
+	['Проезд', 'bus'],
+	['Развлечение', 'puzzle-piece or gamepad'],
+	['Другое',  'meh-o']
+]
+
+income = [
+	['Зарплата', 'credit-card'],
+	['Премия', 'money'],
+	['Пасивный доход', 'download'],
+	['Подарок', 'optin-monster '],
+]
+
+income.each do |name, icon|
+  Category.create( name: name, path_img: icon, "types": :income )
+end
+
+spend.each do |name, icon|
+  Category.create( name: name, path_img: icon, "types": :spending )
+end
